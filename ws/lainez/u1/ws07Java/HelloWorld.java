ae95e80d96a9c6f6127108f6ad7bb506758c56c9
Ricardo-lainez <ricardoandreslainezsuarez@gmail.com>
2024-05-22 14:20:23 -0500

    ws folder V1.0

--- ws/lainez/u1/ws07Java/HelloWorld.java
+++ ws/lainez/u1/ws07Java/HelloWorld.java
@@ -1,12 +1,12 @@
 class HelloWorld{
 	public static void main (String args[]){
-		Sout("Hellow Future engineers from Ricardo");
+		System.out.println("Hellow Future engineers from Ricardo");
 		int addend1 = 3;
 		int addend2 = 5;
 		int sum;
 
 		sum = addend1 + addend2;
 		
-		Sout("the addition of" +addend1+ " + " + addend2 + " is ---> " +sum);
+		System.out.println("the addition of" +addend1+ " + " + addend2 + " is ---> " +sum);
 	}
 }
