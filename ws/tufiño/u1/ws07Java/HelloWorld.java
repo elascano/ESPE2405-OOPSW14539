9cfb3836b11bc2a8042a1ee856874d5ba129b4e2
Erick_Tufiño <eato2001@hotmail.com>
2024-05-22 14:50:09 -0500

    ws folder V1.0

--- ws/tufiño/u1/ws07Java/HelloWorld.java
+++ ws/tufiño/u1/ws07Java/HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("Hello future Engineers form Erick");
-        int addend1 = 5; // Suponiendo que estos son los valores que quieres sumar
+        int addend1 = 5;
         int addend2 = 10;
         int sum = addend1 + addend2;
         
