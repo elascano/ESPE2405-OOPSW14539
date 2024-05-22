a708b20ea204cd381f4d5e3dcae270cf0a50ce8e
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2024-05-22 12:34:56 -0500

    ws folder V1.0

---
+++ ws/blacio/u1/ws07Java/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld{
+    public static void main (String args[]){
+        System.out.println("Hello future Engineers from Julio");
+        int addend1=3;
+        int addend2=5;
+        int sum;
+        sum= addend1+addend2;
+            System.out.println("the addition of " + addend1 + " + " +  addend2 + "  is --> " + sum);
+    }
+}
