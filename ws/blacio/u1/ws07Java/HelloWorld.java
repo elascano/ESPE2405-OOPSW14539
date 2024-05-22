d42e7997dde66edc068c7d257c15786ffc0a0782
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2024-05-22 12:36:41 -0500

    ws folders V1.0

--- ws/blacio/u1/ws07Java/HelloWorld.java
+++ ws/blacio/u1/ws07Java/HelloWorld.java
@@ -5,6 +5,6 @@ class HelloWorld{
         int addend2=5;
         int sum;
         sum= addend1+addend2;
-            System.out.println("the addition of " + addend1 + " + " +  addend2 + "  is --> " + sum);
+            System.out.println("the addition of " + addend1 + " + " +  addend2 + "  is -->  " + sum);
     }
 }
