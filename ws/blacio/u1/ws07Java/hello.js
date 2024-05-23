67f0f35f7d6b12f16b2e52c3dcd0f671e743edd8
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2024-05-22 23:05:10 -0500

    ws folders V1.0

--- ws/blacio/u1/ws07Java/hello.js
+++ ws/blacio/u1/ws07Java/hello.js
@@ -1,8 +1,9 @@
-console.log("Hello, World!");
+console.log("Hello Future Programmers from Julio");
 
 let addend1=3;
 let addend2=5;
 
+
 let sum=addend1+addend2;
 
-console.log("The sum of ",addend1,"and",addend2, "is" , sum);
+console.log("The sum of ",addend1,"and",addend2, "is" , sum); 
