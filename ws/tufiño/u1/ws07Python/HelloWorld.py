6ebc090646804da626915095698b129794ea4095
Erick_Tufiño <eato2001@hotmail.com>
2024-05-22 21:09:45 -0500

    ws folder V1.0

---
+++ ws/tufiño/u1/ws07Python/HelloWorld.py
@@ -0,0 +1,15 @@
+
+def main():
+    print("Hello future Engineers from Erick")
+
+    
+    addend1 = 3
+    addend2 = 5
+    sum = addend1 + addend2
+
+    # Imprimiendo el resultado de la suma
+    print(f"The addition of {addend1} + {addend2} is --> {sum}")
+
+# Llamada a la función principal
+if __name__ == "__main__":
+    main()
