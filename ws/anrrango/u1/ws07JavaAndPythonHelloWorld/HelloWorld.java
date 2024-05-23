f2a70e5e76dab852548e47b2e9e044ef482624c6
Mario-Anrrango <mariosanti280704@gmail.com>
2024-05-22 19:32:58 -0500

    ws folder v1.01

---
+++ ws/anrrango/u1/ws07JavaAndPythonHelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+  public static void main(String args[]){
+   System.out.println("Hello future Engineers from Mario");
+   int addend1 = 3;
+   int addend2 = 5;
+   int sum;
+   sum = addend1 + addend2;
+   System.out.println("The additon is "+sum);
+ }
+}
