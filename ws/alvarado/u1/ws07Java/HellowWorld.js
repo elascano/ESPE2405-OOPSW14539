34e4406b2c65d602ebc3e7de36b2c21835158ad2
Frank Marco Alvarado Grefa <frankmarcogrefa@gmail.com>
2024-05-23 21:02:25 -0500

    ws folders V1.0

---
+++ ws/alvarado/u1/ws07Java/HellowWorld.js
@@ -0,0 +1,8 @@
+console.log("Hello Future Programmers from Frank Alvarado");
+
+let addend1=4;
+let addend2=3;
+
+let sum=addend1+addend2;
+
+console.log("The sum of ",addend1,"and",addend2, "is" , sum); 
