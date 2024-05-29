f4e47c2a987fe053bab440c322827718369dd8ba
Yostin Ariel Sisalema Jiménez <yostinsisalema@gmail.com>
2024-05-29 18:19:55 -0500

    Update ChickenCoop.java

--- ws/sisalema/u1/ws08CD_JavaClasses/ChickenFarmerSimulator/src-ec-edu-espe-farmsimulator/model/ChickenCoop.java
+++ ws/sisalema/u1/ws08CD_JavaClasses/ChickenFarmerSimulator/src-ec-edu-espe-farmsimulator/model/ChickenCoop.java
@@ -9,5 +9,46 @@ package ec.edu.espe.farmsimulator.model;
  * @author Yostin Sisalema,Code Masters, DCCO-ESPE
  */
 public class ChickenCoop {
+    private int id;
+    private Chicken[] chickensCoop; 
 
+    public ChickenCoop(int id, Chicken[] chickensCoop) {
+        this.id = id;
+        this.chickensCoop = chickensCoop;
+    }
+
+public String toString(String type) {
+    StringBuilder chickensType = new StringBuilder();
+    String chickenData = "";
+    if (chickensCoop != null) {
+        for (Chicken chicken : chickensCoop) {
+            chickensType.append(chicken.toString(type)).append("\n");
+        }
+    }
+    if (type.equals("csv")) { // csv
+        chickenData = "ChickenCoop, id=" + id + ", chickensCoop=" + chickensType.toString();
+    } else if (type.equals("txt")) { // txt
+        chickenData = "ChickenCoop: id=" + id + ", chickensCoop=" + chickensType.toString()+"\n";
+    } else if (type.equals("json")) { // json
+        chickenData = "{\"ChickenCoop\": {\"id\": \"" + id + "\", \"chickensCoop\": \"" + chickensType.toString() + "\"}}"+"\n";
+    }
+    return chickenData;
+}
+
+        
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Chicken[] getChickens() {
+        return chickensCoop;
+    }
+
+    public void setChickens(Chicken[] chickens) {
+        this.chickensCoop = chickens;
+    }
 }
