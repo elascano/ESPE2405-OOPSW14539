2f38734b2181eaa344f54272c10bd068b8e8f6a3
Yostin Ariel Sisalema Jiménez <yostinsisalema@gmail.com>
2024-05-29 18:23:59 -0500

    Update Chicken.java

--- ws/sisalema/u1/ws08CD_JavaClasses/ChickenFarmerSimulator/src-ec-edu-espe-farmsimulator/model/Chicken.java
+++ ws/sisalema/u1/ws08CD_JavaClasses/ChickenFarmerSimulator/src-ec-edu-espe-farmsimulator/model/Chicken.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ec.edu.espe.farmsimulator.model;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.Period;
 /**
  *
  * @author Yostin Sisalema,Code Masters, DCCO-ESPE
@@ -14,7 +15,7 @@ public class Chicken {
     private String color;
     private int age;
     private boolean molting;
-    private Date bornOnDate;
+    private LocalDate bornOnDate;
 
 
     @Override
@@ -22,27 +23,33 @@ public class Chicken {
         return "Chicken{" + "id=" + id + ", name=" + name + ", color=" + color + ", age=" + age + ", molting=" + molting + ", BornOnDate=" + BornOnDate + '}';
     }
     
-    public String toString(int type) {
-        String chickenData="";
-        if(type == 1){      //csv
-            chickenData= id + "," + name + "," + color + "," + age + "," + molting + "," + BornOnDate;
-        }
-        if(type == 2){      //txt
-            
-        }
-        if (type == 3){     //json
-           
-        }
-        return chickenData;
-    }
-
-    public Chicken(int id, String name, String color, int age, boolean molting, Date BornOnDate) {
+    public Chicken(int id, String name, String color, boolean molting, LocalDate bornOnDate) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.age = age;
         this.molting = molting;
-        this.BornOnDate = BornOnDate;
+        this.bornOnDate = bornOnDate;
+        this.age = CalculationAge(bornOnDate);
+    }
+    
+     private int CalculationAge(LocalDate bornOnDate) {
+    LocalDate today = LocalDate.now();
+    if (bornOnDate != null) {
+        return Period.between(bornOnDate, today).getYears();
+    } else {
+        return 0;
+    }
+}
+    public String toString(String type) {
+        String chickenData = "";
+        if (type.equals("csv")) { // csv
+            chickenData = id + "," + name + "," + color + "," + CalculationAge(bornOnDate) + "," + molting + "," + getBornOnDate();
+        } else if (type.equals("txt")) { // txt
+            chickenData = "Chicken{id=" + id + ", name=" + name + ", color=" + color + ", age=" + CalculationAge(bornOnDate) + ", molting=" + molting + ", BornOnDate=" + getBornOnDate() + "}";
+        } else if (type.equals("json")) { // json
+            chickenData = "{ \"id\": " + id + ", \"name\": \"" + name + "\", \"color\": \"" + color + "\", \"age\": " + CalculationAge(bornOnDate) + ", \"molting\": " + molting + ", \"BornOnDate\": \"" + getBornOnDate()+ "\" }";
+        }
+        return chickenData;
     }
     /**
      * @return the id
@@ -117,16 +124,16 @@ public class Chicken {
     /**
      * @return the BornOnDate
      */
-    public Date getBornOnDate() {
-        return BornOnDate;
+    public LocalDate getBornOnDate() {
+        return bornOnDate;
     }
 
     /**
-     * @param BornOnDate the BornOnDate to set
+     * @param bornOnDate the bornOnDate to set
      */
-    public void setBornOnDate(Date BornOnDate) {
-        this.BornOnDate = BornOnDate;
-    }
+    public void setBornOnDate(LocalDate bornOnDate) {
+        this.age = CalculationAge(bornOnDate);
+        this.bornOnDate = bornOnDate;
+    }    
 }
-
 
