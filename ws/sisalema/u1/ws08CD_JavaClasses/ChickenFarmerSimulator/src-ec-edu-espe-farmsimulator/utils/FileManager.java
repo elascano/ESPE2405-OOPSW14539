71187ec2c61367e5323c9deeb86d054f0610c048
Yostin Ariel Sisalema Jiménez <yostinsisalema@gmail.com>
2024-05-29 18:18:30 -0500

    Update FileManager.java

--- ws/sisalema/u1/ws08CD_JavaClasses/ChickenFarmerSimulator/src-ec-edu-espe-farmsimulator/utils/FileManager.java
+++ ws/sisalema/u1/ws08CD_JavaClasses/ChickenFarmerSimulator/src-ec-edu-espe-farmsimulator/utils/FileManager.java
@@ -19,9 +19,15 @@ import java.util.Set;
  */
 public class FileManager {
     public static void save(String data, String fileName, String type) throws IOException {
-
-        try (FileWriter writer = new FileWriter(fileName + "." + type, true)) {
-            writer.write(data + "\n");
+        if (!type.equalsIgnoreCase("csv") && !type.equalsIgnoreCase("txt")&&!type.equalsIgnoreCase("json")) {
+            throw new IllegalArgumentException("Unsupported file type: " + type);
+        }if (!fileName.toLowerCase().endsWith("." + type)) {
+            fileName += "." + type;
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            writer.write(data+"\n");
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+            throw e;
         }
     }
-}
