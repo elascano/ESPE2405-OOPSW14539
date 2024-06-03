818fa5ebd9cca64774832f3b5aab583ebb7c8ec9
Ricardo-lainez <ricardoandreslainezsuarez@gmail.com>
2024-06-03 07:36:59 -0500

    folder v1.0

---
+++ dc/Jezhea_SEA_T3/AcademyGradesRegister/AcademyGradesRegister/src/ec/edu/espe/AcademyGradeRegister/model/Student.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AcademyGradeRegister.model;
+
+/**
+ *
+ * @author WINDOWS
+ */
+public class Student {
+    public static void main(String[] args){
+        
+    }
+}
