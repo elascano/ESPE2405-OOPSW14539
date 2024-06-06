d78619aacfb03393520cfe9a61a8b8fb89ddd8f4
Leidy04S <leidysaraguro691@gmail.com>
2024-06-06 13:49:57 -0500

    ws folders V1.0

---
+++ ws/saraguro/u1/WS11 FromClassesToCode/FromClassesToCode/src/ec/edu/espe/fromclassestocode/model/C.java
@@ -0,0 +1,15 @@
+
+package ec.edu.espe.fromclassestocode.model;
+
+/**
+ *
+ * @author Leidy Saraguro,Paradigm Pioneers Squad, DCCO-ESPE
+ */
+public class C {
+    
+    @Override
+    public String toString() {
+        return "C{" + '}';
+    }
+    
+}
