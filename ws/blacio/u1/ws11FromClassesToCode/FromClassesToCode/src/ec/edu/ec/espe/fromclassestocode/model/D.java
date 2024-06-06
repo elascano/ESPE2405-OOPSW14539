f8fbd416aed6ff29b8eceffabf4ec8d4401ef1f4
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2024-06-06 11:07:17 -0500

    ws folders V1.0

---
+++ ws/blacio/u1/ws11FromClassesToCode/FromClassesToCode/src/ec/edu/ec/espe/fromclassestocode/model/D.java
@@ -0,0 +1,15 @@
+
+package ec.edu.ec.espe.fromclassestocode.model;
+
+/**
+ *
+ * @author Julio Blacio, Overnight Developers Squad, DCCO-ESPE
+ */
+public class D {
+
+    @Override
+    public String toString() {
+        return "D{" + '}';
+    }
+
+}
