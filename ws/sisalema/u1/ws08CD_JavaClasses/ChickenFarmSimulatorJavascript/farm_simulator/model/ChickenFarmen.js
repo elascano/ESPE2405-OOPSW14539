1504fd268a2b509ee0a682abe637eeb792e1ca39
Yostin Ariel Sisalema Jiménez <yostinsisalema@gmail.com>
2024-06-24 00:23:38 -0500

    personal folder V1.0

---
+++ ws/sisalema/u1/ws08CD_JavaClasses/ChickenFarmSimulatorJavascript/farm_simulator/model/ChickenFarmen.js
@@ -0,0 +1,52 @@
+export class ChickenFarmen {
+    constructor(id, name, coops) {
+        this.id = id;
+        this.name = name;
+        this.coops = coops;
+    }
+    
+    toString(type) {
+        let chickenFarmer = "";
+        if (this.coops != null) {
+            for (let coop of this.coops) {
+                chickenFarmer += coop.toString(type) + "\n";
+            }
+        }
+        
+        let chickenData = "";
+        if (type === "csv") { // csv
+            chickenData = "ChickenFarmen, id=" + this.id + ", name=" + this.name + ", \n" 
+                        + chickenFarmer;
+        } else if (type === "txt") { // txt
+            chickenData = "ChickenFarmen: id=" + this.id + ", name=" + this.name + ": \n" 
+                        + chickenFarmer + "\n";
+        } else if (type === "json") { // json
+            chickenData = "{\n"
+                        + "  \"ChickenFarmen\": {\n"
+                        + "    \"id\": \"" + this.id + "\",\n"
+                        + "    \"name\": \"" + this.name + "\",\n"
+                        + "    \"chickenFarmer\": [\n"
+                        + chickenFarmer + "\n"
+                        + "    ]\n"
+                        + "  }\n"
+                        + "}\n";
+        }
+        return chickenData;
+    }
+    
+    getId() {
+        return this.id;
+    }
+
+    setId(id) {
+        this.id = id;
+    }
+
+    getCoops() {
+        return this.coops;
+    }
+
+    setCoops(coops) {
+        this.coops = coops;
+    }
+}
