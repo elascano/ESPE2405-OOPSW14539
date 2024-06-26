867d3bc5b925d6aa226331a32173680587b0b40d
Frank Marco Alvarado Grefa <frankmarcogrefa@gmail.com>
2024-06-26 08:48:18 -0500

    wsandwk

---
+++ ws/alvarado/u1/ws11FromClassesToCode/FromClassesToCode/src/main/java/ec/edu/ec/espe/fromclassestocode/model/C.java
@@ -0,0 +1,13 @@
+
+package ec.edu.ec.espe.fromclassestocode.model;
+
+/**
+ *
+ * @author Frank Alvaradom, As-Byte Wizards, DCCO-ESPE
+ */
+public class C {
+      @Override
+    public String toString() {
+        return "C{" + '}';
+    }
+}
---
+++ ws/alvarado/u1/ws11FromClassesToCode/FromClassesToCode/src/main/java/ec/edu/ec/espe/fromclassestocode/model/E.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ec.espe.fromclassestocode.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Frank Alvaradom, As-Byte Wizards, DCCO-ESPE
+ */
+public class E {
+    private ArrayList<C> cs;
+
+    @Override
+    public String toString() {
+        return "E{" + "cs=" + cs + '}';
+    }
+
+    public E(ArrayList<C> cs) {
+        this.cs = cs;
+    }
+
+    /**
+     * @return the cs
+     */
+    public ArrayList<C> getCs() {
+        return cs;
+    }
+
+    /**
+     * @param cs the cs to set
+     */
+    public void setCs(ArrayList<C> cs) {
+        this.cs = cs;
+    }
+}
