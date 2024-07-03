c8d4aa46f1332259dbd0c3d5f23b27f942d264be
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2024-07-03 15:10:52 -0500

    hw folders V2.0

---
+++ dc/Overnight_Development_Squad_T2/GeneratedUML/MongoDBFiles/src/main/java/com/mycompany/mongodbfiles/MongoDBFiles.java
@@ -0,0 +1,16 @@
+package org.xulescode.mongodb;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+package com.mycompany.mongodbfiles;
+
+/**
+ *
+ * @author Julio Blacio, Overnight Developers Squad, DCCO-ESPE
+ */
+public class MongoDBFiles {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
