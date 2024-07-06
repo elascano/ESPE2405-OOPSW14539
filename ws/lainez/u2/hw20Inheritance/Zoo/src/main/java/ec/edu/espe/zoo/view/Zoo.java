84d37e969fee3d1636c9c6bc8768284638856229
Ricardo-lainez <ricardoandreslainezsuarez@gmail.com>
2024-07-06 17:09:34 -0500

    folder V1.0

---
+++ ws/lainez/u2/hw20Inheritance/Zoo/src/main/java/ec/edu/espe/zoo/view/Zoo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.zoo.view;
+
+import ec.edu.espe.zoo.model.Lion;
+
+/**
+ *
+ * @author WINDOWS
+ */
+public class Zoo {
+    public static void main(String[] args) {
+        System.out.println("hellow inheritance!");
+        Lion lion = new Lion();
+        
+        lion.register(3);
+    }
+}
