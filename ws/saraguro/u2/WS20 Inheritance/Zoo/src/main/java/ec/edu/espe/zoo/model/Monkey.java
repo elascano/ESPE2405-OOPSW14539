f60b8a81f2ab93b9273cc1eaf483c00f3e445c86
Leidy04S <leidysaraguro691@gmail.com>
2024-07-09 17:01:49 -0500

    .git

---
+++ ws/saraguro/u2/WS20 Inheritance/Zoo/src/main/java/ec/edu/espe/zoo/model/Monkey.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.zoo.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Leidy Saraguro,Paradigm Pioneers Squad, DCCO-ESPE
+ */
+public class Monkey extends Primate {
+
+    public Monkey(int id, Date bornOnDate, Cage cage, char gender, int numberOfLegs) {
+        super(id, bornOnDate, cage, gender, numberOfLegs);
+    }
+
+    @Override
+    public void feed(int food) {
+        System.out.println("feeding a monkey with" + food + "fruits");
+    }
+
+}
