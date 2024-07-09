e35db28656f4ea8389e5471a5697a2b8b317618b
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2024-07-09 15:30:57 -0500

    ws folders V2.0

--- ws/blacio/u2/ws20Inheritance/Zoo/src/main/java/ec/edu/espe/zoo/model/Condor.java
+++ ws/blacio/u2/ws20Inheritance/Zoo/src/main/java/ec/edu/espe/zoo/model/Condor.java
@@ -1,14 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package ec.edu.espe.zoo.model;
+
+import java.util.Date;
 
 /**
  *
  * @author Julio Blacio, Overnight Developers Squad, DCCO-ESPE
  */
 public class Condor extends Bird {
+
+    private String mountain;
+
+    public Condor(String mountain, int id, Date bornOnDate, Cage cage, char gender, int numberOfLegs) {
+        super(id, bornOnDate, cage, gender, numberOfLegs);
+        this.mountain = mountain;
+    }
+
+    @Override
+    public String toString() {
+        return "Condor{" + "mountain=" + mountain + '}' + super.toString();
+    }
+
+    @Override
+    public void feed(int food) {
+        System.out.println("Feeding a Condor with " + food + "pounds of meat");
+    }
+
+    /**
+     * @return the mountain
+     */
+    public String getMountain() {
+        return mountain;
+    }
+
+    /**
+     * @param mountain the mountain to set
+     */
+    public void setMountain(String mountain) {
+        this.mountain = mountain;
+    }
 
 }
--- ws/blacio/u2/ws20Inheritance/Zoo/src/main/java/ec/edu/espe/zoo/model/Eagle.java
+++ ws/blacio/u2/ws20Inheritance/Zoo/src/main/java/ec/edu/espe/zoo/model/Eagle.java
@@ -5,10 +5,27 @@
 
 package ec.edu.espe.zoo.model;
 
+import java.util.Date;
+
 /**
  *
  * @author Julio Blacio, Overnight Developers Squad, DCCO-ESPE
  */
 public class Eagle extends Bird {
 
+    public Eagle(int id, Date bornOnDate, Cage cage, char gender, int numberOfLegs) {
+        super(id, bornOnDate, cage, gender, numberOfLegs);
+        region=3;
+    }
+
+    @Override
+    public void feed(int food) {
+        System.out.println("Feeding an eagle with " + food + "mice");
+    }
+  
+    //Overloading
+    public void feed(Food food){
+        System.out.println("feeding an eagle with" + food);
+    }
+   
 }
