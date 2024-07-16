e4d93e34bdb759f46923455febcc972a3415980b
Jerson Stiven Llumiquinga Moreno <jersonllumiquingam23@gmail.com>
2024-07-15 23:25:42 -0500

    up
    uup

---
+++ ws/llumiquinga/u2/ws26InterfacetoMongoDB/ZooGUIToMongoDB/src/main/java/ec/edu/espe/zoo/controller/AnimalController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.zoo.controller;
+
+import ec.edu.espe.zoo.model.Animal;
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author Jerson Llumiquinga M. - TEAM: JEZHEA S.E.A
+ */
+public class AnimalController {
+
+    }
