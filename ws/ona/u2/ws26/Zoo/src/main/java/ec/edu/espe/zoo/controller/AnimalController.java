ceb1b1fec3edac53ce0e8d29eda7284c32b839e1
leydiespe <espelady34@gmail.com>
2024-07-18 22:51:31 -0500

    v1.0

---
+++ ws/ona/u2/ws26/Zoo/src/main/java/ec/edu/espe/zoo/controller/AnimalController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.zoo.controller;
+
+import ec.edu.espe.zoo.model.Animal;
+
+/**
+ *
+ * @author Leydi Oña
+ */
+public class AnimalController {
+    public boolean create(Animal animal){
+        
+        return true;
+    }
+}
