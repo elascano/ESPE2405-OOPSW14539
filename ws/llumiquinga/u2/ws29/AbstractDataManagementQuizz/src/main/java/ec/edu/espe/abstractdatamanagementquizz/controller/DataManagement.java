a58fd307ce8ed471a91d877991ee9cec52dfe3f5
Jerson Stiven Llumiquinga Moreno <jersonllumiquingam23@gmail.com>
2024-07-28 01:23:45 -0500

    up up up

---
+++ ws/llumiquinga/u2/ws29/AbstractDataManagementQuizz/src/main/java/ec/edu/espe/abstractdatamanagementquizz/controller/DataManagement.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.abstractdatamanagementquizz.controller;
+
+
+
+/**
+ *
+ * @author Jerson Llumiquinga M. - TEAM: JEZHEA S.E.A
+ */
+public abstract class DataManagement {//2
+        public boolean isConnected (){
+            
+            return true;
+        } 
+    
+}
