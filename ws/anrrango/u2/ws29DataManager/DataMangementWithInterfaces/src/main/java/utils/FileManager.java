fd9fbfde5e6085837ab866b89707b27260f5599b
Mario-Anrrango <mariosanti280704@gmail.com>
2024-07-30 10:34:17 -0500

    ws folder

---
+++ ws/anrrango/u2/ws29DataManager/DataMangementWithInterfaces/src/main/java/utils/FileManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author Molina Gallegos Gabriel Anthony, CodeCrafting Engineers
+ */
+public interface FileManager  extends DataManagement{
+    public void openFile (String fileName);
+    public void closeFile (String fileName);
+}
