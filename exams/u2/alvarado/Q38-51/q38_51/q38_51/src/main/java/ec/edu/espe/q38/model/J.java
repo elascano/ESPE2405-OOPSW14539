6666a2fc1b75dc791ea55fb4a0594e759a34e6ba
Frank Marco Alvarado Grefa <frankmarcogrefa@gmail.com>
2024-08-03 06:30:11 -0500

    updat

---
+++ exams/u2/alvarado/Q38-51/q38_51/q38_51/src/main/java/ec/edu/espe/q38/model/J.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.q38.model;
+
+
+public class J { // 1.0
+    
+}
--- exams/u2/alvarado/Q38-51/src/ec/edu/espe/q3851/model/J.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.espe.q3851.model;
-
-/**
- *
- * @author Jairo Bonilla, Developer Bears, DCCO-ESPE
- */
-public class J {
-    
-}
