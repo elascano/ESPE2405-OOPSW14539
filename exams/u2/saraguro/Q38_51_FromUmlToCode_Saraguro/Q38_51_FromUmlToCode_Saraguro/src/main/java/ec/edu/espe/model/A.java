b473b5f47def13ede2fbbe254d40d37277cd09e3
Leidy04S <leidysaraguro691@gmail.com>
2024-08-03 07:16:20 -0500

    Exam 2

--- exams/u2/saraguro/Q38_51_FromUmlToCode_Saraguro/Q38_51_FromUmlToCode_Saraguro/src/main/java/ec/edu/espe/model/A.java
+++ exams/u2/saraguro/Q38_51_FromUmlToCode_Saraguro/Q38_51_FromUmlToCode_Saraguro/src/main/java/ec/edu/espe/model/A.java
@@ -1,10 +1,39 @@
 
 package ec.edu.espe.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Leidy Saraguro, Paradigm Pioneers Squad, DCCO-ESPE
  */
-public class A {
+public class A {        
+    private ArrayList<A> as;    
+
+    //0.2
+    
+    /**
+     * @return the as
+     */
+    public ArrayList<A> getAs() {
+        return as;
+    }
+
+    /**
+     * @param as the as to set
+     */
+    public void setAs(ArrayList<A> as) {
+        this.as = as;
+    }
+
+    public A(ArrayList<A> as) {
+        this.as = as;
+    }
+
+    @Override
+    public String toString() {
+        return "A{" + "as=" + as + '}';
+    }
+    
     
 }
