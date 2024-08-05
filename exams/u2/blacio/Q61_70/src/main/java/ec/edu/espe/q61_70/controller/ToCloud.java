ea077203f3f36fac07260163dab1828e574907c9
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2024-08-05 15:50:59 -0500

    exams folders v2.0

--- exams/u2/blacio/Q61_70/src/main/java/ec/edu/espe/q61_70/controller/ToCloud.java
+++ exams/u2/blacio/Q61_70/src/main/java/ec/edu/espe/q61_70/controller/ToCloud.java
@@ -9,10 +9,14 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import ec.edu.espe.q61_70.model.Keyboard;
+import java.util.ArrayList;
+import java.util.List;
 import org.bson.Document;
 
 public class ToCloud {
@@ -60,5 +64,66 @@ public class ToCloud {
         } catch (MongoException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Keyboard findKeyboardById(int id) {
+        Keyboard keyboard = null;
+        try (MongoClient mongoClient = createMongoClient()) {
+            MongoDatabase database = mongoClient.getDatabase("KeyboardDatabase");
+            MongoCollection<Document> collection = database.getCollection("keyboards");
+
+            Document query = new Document("id", id);
+            FindIterable<Document> result = collection.find(query);
+
+            Document document = result.first();
+            if (document != null) {
+                int dbId = document.getInteger("id");
+                String name = document.getString("name");
+                float price = document.getDouble("price").floatValue();
+                float weight = document.getDouble("weight").floatValue();
+                int amount = document.getInteger("amount");
+
+                keyboard = new Keyboard(dbId, name, price, weight, amount);
+            }
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
+        return keyboard;
+    }
+
+    public static void deleteKeyboardById(int id) {
+        try (MongoClient mongoClient = createMongoClient()) {
+            MongoDatabase database = mongoClient.getDatabase("KeyboardDatabase");
+            MongoCollection<Document> collection = database.getCollection("keyboards");
+
+            Document query = new Document("id", id);
+            collection.deleteOne(query);
+            System.out.println("Keyboard deleted successfully!");
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Keyboard> getAllKeyboards() {
+        List<Keyboard> keyboards = new ArrayList<>();
+        try (MongoClient mongoClient = createMongoClient()) {
+            MongoDatabase database = mongoClient.getDatabase("KeyboardDatabase");
+            MongoCollection<Document> collection = database.getCollection("keyboards");
+
+            FindIterable<Document> result = collection.find();
+            for (Document document : result) {
+                Keyboard keyboard = new Keyboard(
+                        document.getInteger("id"),
+                        document.getString("name"),
+                        document.getDouble("price").floatValue(),
+                        document.getDouble("weight").floatValue(),
+                        document.getInteger("amount")
+                );
+                keyboards.add(keyboard);
+            }
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
+        return keyboards;
     }
 }
