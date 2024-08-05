4b52ca54c8c1d998d9cfddef8275d56b9ff96df3
Yostin Ariel Sisalema Jiménez <yostinsisalema@gmail.com>
2024-08-05 15:02:48 -0500

    HW folder

---
+++ ws/sisalema/u2/ws29DataManger/AbstractDataManagementQuiz/src/main/java/ec/edu/espe/abstractdatamanagementquiz/controller/CsvManager.java
@@ -0,0 +1,11 @@
+
+package ec.edu.espe.abstractdatamanagementquiz.controller;
+
+/**
+ *
+ * @author Yostin Sisalema, Code Master , DCCO-ESPE
+ */
+public class CsvManager extends FileManager { //2
+
+    
+}
---
+++ ws/sisalema/u2/ws30UnitTestingInPairs/FisicaUnitTest/src/main/java/ec/edu/espe/fisicaunittest/utils/kineticEnergy.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ec.edu.espe.fisicaunittest.utils;
+
+/**
+ *
+ * @author Yostin Sisalema, Code Master , DCCO-ESPE
+ */
+public class kineticEnergy {
+    
+    public static double calculatekineticEnergy(double mass, double velocity) {
+        // E_k = (1/2) * m * v^2
+        
+        return 0.5 * mass * velocity * velocity;
+}
+}
