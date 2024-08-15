2392c971f3df857a394df90ebb2052807a270b44
Julio César Blacio Machuca <jcblaciomachuca@gmail.com>
2024-08-15 00:33:30 -0500

    hw folders V3.0

---
+++ hw/blacio/u3/HW20 MazeGeneration/Mgs/src/main/java/ec/edu/espe/mgs/model/Wall.java
@@ -0,0 +1,36 @@
+package ec.edu.espe.mgs.model;
+
+import java.util.Optional;
+
+/**
+ *
+ * @author Julio Blacio, Overnight Developers Squad, DCCO-ESPE
+ */
+public class Wall {
+
+    private boolean isExternal;
+    private Optional<Room> room1;
+    private Optional<Room> room2;
+
+    public Wall(boolean isExternal) {
+        this.isExternal = isExternal;
+        this.room1 = Optional.empty();
+        this.room2 = Optional.empty();
+    }
+
+    public void setAsExternal() {
+        this.isExternal = true;
+    }
+
+    public void addAdjacentRoom(Room room) {
+        if (room1.isEmpty()) {
+            room1 = Optional.of(room);
+        } else {
+            room2 = Optional.of(room);
+        }
+    }
+
+    public boolean isExternalWall() {
+        return isExternal;
+    }
+}
