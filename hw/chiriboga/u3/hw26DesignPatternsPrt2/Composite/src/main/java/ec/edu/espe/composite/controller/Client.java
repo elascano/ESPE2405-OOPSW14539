b972c260b4ce3510955f406d5254403371123a50
k0c0h <chiribogakerly@gmail.com>
2024-08-22 14:13:22 -0500

    personal folder V1.0

---
+++ hw/chiriboga/u3/hw26DesignPatternsPrt2/Composite/src/main/java/ec/edu/espe/composite/controller/Client.java
@@ -0,0 +1,17 @@
+
+package ec.edu.espe.composite.controller;
+
+import ec.edu.espe.composite.model.Employee;
+
+/**
+ *
+ * @author Kerlly Chiriboga, ODS
+ */
+public class Client {
+    public static Employee employee;
+
+    public static void doClientTasks() {
+        employee.stateName();
+    }
+}
+
