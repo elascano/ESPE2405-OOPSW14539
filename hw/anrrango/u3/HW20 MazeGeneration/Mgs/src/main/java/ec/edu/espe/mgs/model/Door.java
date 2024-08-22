72ff8e6793dfd687a810f629ed980a1ab8efe4aa
Mario-Anrrango <mariosanti280704@gmail.com>
2024-08-22 14:25:47 -0500

    Hw Folder

---
+++ hw/anrrango/u3/HW20 MazeGeneration/Mgs/src/main/java/ec/edu/espe/mgs/model/Door.java
@@ -0,0 +1,35 @@
+package ec.edu.espe.mgs.model;
+
+/**
+ *
+ * @author Mario Anrrango, A-Byte Wizards, DCCO-ESPE
+ */
+public class Door {
+
+    private Room room1;
+    private Room room2;
+    private boolean isOpen;
+
+    public Door(Room room1, Room room2) {
+        this.room1 = room1;
+        this.room2 = room2;
+        this.isOpen = true; // By default, doors are open
+    }
+
+    public Room getOtherRoom(Room currentRoom) {
+        if (currentRoom.equals(room1)) {
+            return room2;
+        } else if (currentRoom.equals(room2)) {
+            return room1;
+        }
+        throw new IllegalArgumentException("Room not connected by this door.");
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+}
