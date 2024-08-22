27445bc8bba02aba8f6b8f66b02fa07d5d009733
Miguel Caiza <miguel14caiza@gmail.com>
2024-08-22 11:06:47 -0500

    Abstract Factory

---
+++ hw/caiza/u3/HW25AbstractFactory/GUIFactory/src/main/java/ec/edu/espe/guifactory/model/LinuxFactory.java
@@ -0,0 +1,20 @@
+package ec.edu.espe.guifactory.model;
+
+import java.awt.Button;
+import java.awt.Menu;
+
+/**
+ *
+ * @author Miguel Caiza,Overnight Developers Squad,DCCO-ESPE
+ */
+public class LinuxFactory extends GUIFactory {
+    @Override
+    public Button createButton() {
+        return new LinuxButton();
+    }
+
+    @Override
+    public Menu createMenu() {
+        return new LinuxMenu();
+    }  
+}
---
+++ hw/caiza/u3/HW25AbstractFactory/GUIFactory/src/main/java/ec/edu/espe/guifactory/view/ClientApp.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.guifactory.view;
+import ec.edu.espe.guifactory.controller.ApplicationController;
+
+/**
+ *
+ * @author Miguel Caiza,Overnight Developers Squad,DCCO-ESPE
+ */
+public class ClientApp {
+    public static void main(String[] args) {
+        ApplicationController controller = new ApplicationController();
+        controller.run();
+    }    
+}
