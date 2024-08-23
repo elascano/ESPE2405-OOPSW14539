a3bf843b20b1da6dbdf43a7e0bdc27074a5b6cd8
Ricardo Andres Laínez Suárez <ricardoandreslainezsuarez@gmail.com>
2024-08-22 23:09:04 -0500

    folder V1.0

---
+++ hw/lainez/u3/hw27StrategyPattern/StrategyPatttern/src/main/java/ec/edu/espe/strategypattern/controller/SortingStrategy.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ec.edu.espe.strategypattern.controller;
+
+/**
+ *
+ * @author Lainez Ricardo JEZHE ESPE
+ */
+public interface SortingStrategy {
+
+    public int[] sort(int[] data);
+    
+}
