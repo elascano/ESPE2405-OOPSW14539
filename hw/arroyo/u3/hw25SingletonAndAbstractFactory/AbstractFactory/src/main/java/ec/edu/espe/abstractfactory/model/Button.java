b3d24a4d7dd4a2ba5e234aeec075f141049b3bb6
Alfonso Alejandro Arroyo Paredes <arroyoalfonso1031@gmail.com>
2024-08-23 01:19:59 -0500

    hws

---
+++ hw/arroyo/u3/hw25SingletonAndAbstractFactory/AbstractFactory/src/main/java/ec/edu/espe/abstractfactory/model/Button.java
@@ -0,0 +1,10 @@
+package ec.edu.espe.abstractfactory.model;
+
+/**
+ *
+ * @author Kerlly Chiriboga, ODS
+ */
+public abstract class Button {
+    public String caption;
+    public abstract void paint();
+}
